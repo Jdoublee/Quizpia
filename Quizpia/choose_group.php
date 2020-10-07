b3a3bde20f63d30940a8590927541b4750715c7a
Jdoublee <35803181+Jdoublee@users.noreply.github.com>
2020-10-07 23:56:17 +0900

    Update choose_group.php

--- Quizpia/choose_group.php
+++ Quizpia/choose_group.php
@@ -3,7 +3,7 @@
 <html>
 <head>
     <title>choose group</title>
-    <meta charset="utf-8">   
+    <meta charset="utf-8" />   
     <!--<script>
         function tochoosetype(){
             window.location.href="ch.php";
@@ -19,23 +19,23 @@
 
     -->
 
-    <form name="choose_group" action="choose_quiz_type.php" method="post" >
- 
-    <div id="opt1">
-    <input type="radio" value="1" name="group" > 그룹 1
-    </div>
+<form name="choose_group" action="choose_quiz_type.php" method="post" >
 
-    <div id="opt2">
-    <input type="radio" value="2" name="group"> 그룹 2
-    </div>
+<div id="opt1">
+<input type="radio" value="1" name="group" /> 그룹 1
+</div>
 
-    <div id="opt3">
-    <input type="radio" value="3" name="group"> 그룹 3
-    </div>
+<div id="opt2">
+<input type="radio" value="2" name="group" /> 그룹 2
+</div>
 
-    <input type="submit" value="그룹 선택">
+<div id="opt3">
+<input type="radio" value="3" name="group" /> 그룹 3
+</div>
 
-    </form>
+<input type="submit" value="그룹 선택">
+
+</form>
 
 <!--만들어진 그룹 수만큼 버튼 존재-->
 </body>
